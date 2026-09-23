94e259f3bdadedd4e486aab98a43536c7651b4e0
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-09-23 17:17:40 +0530

    fix: correct DOCX office relationship type

--- assets/js/pdf-to-word.js
+++ assets/js/pdf-to-word.js
@@ -74,7 +74,7 @@ function buildDocx(pages) {
   zip.file("_rels/.rels",
     '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
     '<Relationships xmlns="' + REL + '">' +
-      '<Relationship Id="rId1" Type="' + REL + '/officeDocument" Target="word/document.xml"/>' +
+      '<Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml"/>' +
       '<Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/>' +
       '<Relationship Id="rId3" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/>' +
     "</Relationships>"
